048c5658ae53bdd270b656df779570f9b2b7f195
ElectroGeorge7 <gkat1856ntin@yandex.ru>
2023-02-02 13:05:08 +0300

    try test activity menu
    Try to reliaze test activity menu, but it`s some problems in it.

--- Menu/test_activity.c
+++ Menu/test_activity.c
@@ -17,8 +17,9 @@
 #define TEST_ACT_CONFIG_IS_SET 1
 static uint8_t testActStatusFlags = 0;
 
-
-static uint8_t curLcdRow = 0;
+#define TEST_ACT_MENU_ROW_NUM 22
+static uint8_t curMenuRow = 0;
+static char testActMenu[TEST_ACT_MENU_ROW_NUM][16] = {0};
 
 typedef enum TestType{
 	RELIABILITY_TEST,
@@ -43,8 +44,8 @@ typedef enum ResCheckMethod{
 
 typedef struct {
 	TestType_t testType;
-	char *partNumber[16];
-	char *mldrNum[4];
+	char partNumber[16];
+	char mldrNum[8];
 	//uint32_t curResNum;		// number of current displayed result
 	uint8_t cellNum;
 	uint8_t	rowNum;
@@ -58,7 +59,21 @@ typedef struct {
 
 HAL_StatusTypeDef test_view_update(Command_t testAction, uint8_t *data){
 	char pBuf[16] = {0};
-	DataTime_t dataTime;
+	DataTime_t dataTime = {0};
+
+	TestConfig_t curConfig = {
+		RELIABILITY_TEST,
+		"qwerty1234",
+		"MLDR123",
+		12,
+		4,
+		3,
+		EVERY_RESULT,
+		{0, 30, 14, 12, 2, 2023 },
+		10000,
+		1,
+		1
+	};
 
 	int hour = 0;
 	int min = 0;
@@ -71,8 +86,34 @@ HAL_StatusTypeDef test_view_update(Command_t testAction, uint8_t *data){
 			;
 			break;
         case UP_CMD:
+			if ( curMenuRow != 0 ){
+				curMenuRow--;
+				if ( curMenuRow%2 ){
+					LCD_SetCursor( 15, 0 );
+				}else{
+					LCD_Clear();
+					LCD_SetCursor( 0, 0 );
+					LCD_PrintString(testActMenu[curMenuRow-1]);
+					LCD_SetCursor( 0, 1 );
+					LCD_PrintString(testActMenu[curMenuRow]);
+					LCD_SetCursor( 15, 1 );
+				}
+			}
             break;
         case DOWN_CMD:
+        	if ( (curMenuRow+1) < TEST_ACT_MENU_ROW_NUM){
+        		curMenuRow++;
+				if ( curMenuRow%2 ){
+					LCD_SetCursor( 15, 1 );
+				}else{
+					LCD_Clear();
+					LCD_SetCursor( 0, 0 );
+					LCD_PrintString(testActMenu[curMenuRow]);
+					LCD_SetCursor( 0, 1 );
+					LCD_PrintString(testActMenu[curMenuRow+1]);
+					LCD_SetCursor( 15, 0 );
+				}
+        	}
             break;
 		case SELECT_CMD:
 
@@ -87,6 +128,29 @@ HAL_StatusTypeDef test_view_update(Command_t testAction, uint8_t *data){
 					LCD_PrintString("...");
 					HAL_Delay(1000);
 
+
+#ifdef TEST_ACT
+					LCD_PrintString(curConfig.partNumber);
+					LCD_PrintString("Рез.№:"); LCD_PrintString('0');
+					LCD_PrintString(curConfig.mldrNum); LCD_PrintString(curConfig.cellNum); LCD_PrintString("шт.");
+
+					LCD_PrintString("row:"); LCD_PrintString(curConfig.rowNum); LCD_PrintString("col:"); LCD_PrintString(curConfig.colNum);
+					LCD_PrintString("Отобр-ие рез-та:");
+					LCD_PrintString(curConfig.resCheckMethod);
+					LCD_PrintString("тек. время:");
+					LCD_PrintString(curDateTime);
+					LCD_PrintString("начало испыт-ия:");
+					LCD_PrintString(curConfig.testStartDataTime);
+					LCD_PrintString("конец испыт-ия:");
+					LCD_PrintString(curConfig.testStartDataTime + curConfig.testDurationInHours);
+					LCD_PrintString("осталось:");
+					LCD_PrintString(curConfig.testStartDataTime + curConfig.testDurationInHours - curDateTime);
+
+					LCD_PrintString("кол-во Vпит:"); LCD_PrintString(curConfig.powerSupplyNum);
+					LCD_PrintString("темп.№1:"); LCD_PrintString(temp1); LCD_PrintString('C');
+					LCD_PrintString("темп.№2:"); LCD_PrintString(temp2); LCD_PrintString('C');
+					LCD_PrintString("кол-во плат:"); LCD_PrintString(curConfig.pcbNum);
+#endif
 					if (0/* check config file in file system */){
 						// get configuration from the file
 						testActStatusFlags |= TEST_ACT_CONFIG_IS_SET;
@@ -95,6 +159,62 @@ HAL_StatusTypeDef test_view_update(Command_t testAction, uint8_t *data){
 						LCD_PrintString("не найдено");
 						HAL_Delay(1000);
 
+						//testActMenu[0] = curConfig.partNumber;
+						//testActMenu[1] = "Рез.№:0";
+						memcpy(testActMenu[0], curConfig.partNumber, sizeof(curConfig.partNumber));
+						memcpy(testActMenu[1], "Рез.:0", sizeof("Рез.:0"));
+
+						snprintf(testActMenu[2], 16, "%s  %dшт.", curConfig.mldrNum, curConfig.cellNum);
+						snprintf(testActMenu[3], 16, "row:%d    col:%d", curConfig.rowNum, curConfig.colNum);
+
+						//testActMenu[4] = "Испытание:";
+						memcpy(testActMenu[4], "Испытание:", 16);
+						snprintf(testActMenu[5], 16, "%s", curConfig.testType ? "ЭТТ" : "Безотказность");
+
+
+						//testActMenu[6] = "Отобр-ие рез-та:";
+						memcpy(testActMenu[6], "Отобр-ие рез-та:", 16);
+						snprintf(testActMenu[7], 16, "%d", curConfig.resCheckMethod);
+
+						//testActMenu[8] = "тек. время:";
+						memcpy(testActMenu[8], "тек. время:", 16);
+						rtc_get(&dataTime);
+						snprintf(testActMenu[9], 16, "%d:%d %d.%d.%d", dataTime.hour, dataTime.min, dataTime.day, dataTime.mon, dataTime.year);
+
+						//testActMenu[10] = "начало испыт-ия:";
+						memcpy(testActMenu[10], "начало испыт-ия:", 16);
+						memcpy(&(curConfig.testStartDataTime), &dataTime, sizeof(DataTime_t));
+						snprintf(testActMenu[11], 16, "%d:%d %d.%d.%d", dataTime.hour, dataTime.min, dataTime.day, dataTime.mon, dataTime.year);
+
+						//testActMenu[12] = "конец испыт-ия:";
+						memcpy(testActMenu[12], "конец испыт-ия:", 16);
+						snprintf(testActMenu[13], 16, "%d:%d %d.%d.%d", 5, 0, 7, 5, 2023);
+
+						//testActMenu[14] = "осталось:";
+						memcpy(testActMenu[14], "осталось:", 16);
+						snprintf(testActMenu[15], 16, "%d:%d %d.%d.%d", 5, 3, 2, 3, 0);
+
+						snprintf(testActMenu[16], 16, "кол-во Vпит:%d", curConfig.powerSupplyNum);
+						float temp1 = Max6675_Read_Temp();
+						float temp2 = Max6675_Read_Temp();
+						memset(testActMenu[17], 0, 16);
+
+						snprintf(testActMenu[18], 16, "темп.№1:%3.0fC", temp1);
+						snprintf(testActMenu[19], 16, "темп.№2:%3.0fC", temp2);
+
+						snprintf(testActMenu[20], 16, "кол-во плат:%d", curConfig.pcbNum);
+						memset(testActMenu[21], 0, 16);
+
+						LCD_Clear();
+						curMenuRow = 0;
+
+						LCD_SetCursor( 0, 0 );
+						LCD_PrintString(testActMenu[0]);
+						LCD_SetCursor( 0, 1 );
+						LCD_PrintString(testActMenu[1]);
+
+						LCD_CursorOnOff(1);
+						LCD_SetCursor( 15, 0 );
 
 						// enter config by usb cdc
 						testActStatusFlags |= TEST_ACT_CONFIG_IS_SET;
